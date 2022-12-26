157eac713819f5eef8330b3a6b9f400e2627bc54
Rene Saarsoo <nene@triin.net>
2022-12-26 19:08:11 +0200

    Display workout in nice popup

--- tr-content.js
+++ tr-content.js
@@ -1,6 +1,33 @@
 browser.runtime.onMessage.addListener((workout) => {
-  console.log(trainerroadToZwiftout(workout));
+  createPopup(trainerroadToZwiftout(workout));
 });
+
+function createPopup(message) {
+  const div = document.createElement("div");
+  div.style.position = "absolute";
+  div.style.top = "0";
+  div.style.left = "200px";
+  div.style.right = "200px";
+  div.style.width = "800px";
+  div.style.border = "1px solid gray";
+  div.style.background = "white";
+
+  const btn = document.createElement("button");
+  btn.style.position = "absolute";
+  btn.style.top = "2px";
+  btn.style.right = "2px";
+  btn.innerHTML = "X";
+  btn.onclick = () => {
+    document.body.removeChild(div);
+  };
+  div.appendChild(btn);
+
+  const pre = document.createElement("pre");
+  pre.innerHTML = message;
+  div.appendChild(pre);
+
+  document.body.appendChild(div);
+}
 
 function trainerroadToZwiftout(json) {
   const timeData = json.Workout.workoutData;
